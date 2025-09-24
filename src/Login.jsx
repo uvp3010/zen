fe9688cdc3ca869d659f5125a2536d08a1e7bce7
uvp3010 <richardalpert3010@gmail.com>
2025-09-24 16:58:01 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "https://zenback-3.onrender.com/login",
+        "https://zenback.onrender.com/login",
         { username, password },
         { withCredentials: true }
       );
